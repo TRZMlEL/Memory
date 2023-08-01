4315612eadc1604fb702a74fdfd631257c445b13
TRZMlEL <mateusztrzmiel2104@gmail.com>
2023-08-01 17:14:00 +0200

    modified:   game.js 	modified:   style.css

--- game.js
+++ game.js
@@ -1,1 +1,20 @@
-console.log("hello world")
+let START = document.querySelector('#STARTbtn');
+let body = document.body;
+let cards = document.querySelectorAll('.card');
+let backs = document.querySelectorAll('.back');
+let card, back;
+
+START.addEventListener('click', () => {
+    START.style.visibility = 'hidden';
+    cards.forEach(card => {
+        card.style.position = 'relative';
+        card.style.width = '30vmin';
+        card.style.height = '45vmin';
+        card.style.top = '0%';
+    }); 
+    backs.forEach(back=> {
+        back.style.fontsize = '20vmin';
+    });
+    body.style.alignItems = 'stretch';
+    body.style.justifyContent = 'space-around'
+});
