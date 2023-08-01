178cb9e5562b7066480ae2c062012cb25445bfb7
TRZMlEL <mateusztrzmiel2104@gmail.com>
2023-08-01 19:25:09 +0200

    modified:   game.js 	modified:   index.html 	modified:   style.css

--- game.js
+++ game.js
@@ -4,6 +4,8 @@ let cards = document.querySelectorAll('.card');
 let backs = document.querySelectorAll('.back');
 let card, back;
 
+let cardClicked = true
+
 START.addEventListener('click', () => {
     START.style.visibility = 'hidden';
     cards.forEach(card => {
@@ -13,8 +15,40 @@ START.addEventListener('click', () => {
         card.style.top = '0%';
     }); 
     backs.forEach(back=> {
-        back.style.fontsize = '20vmin';
+        back.style.fontSize = '5vmin';
     });
     body.style.alignItems = 'stretch';
-    body.style.justifyContent = 'space-around'
 });
+
+let flippedCards = [];
+
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+    // Sprawdzenie, czy karta jest już obrócona
+    if (flippedCards.includes(card)) {
+      // Karta jest już obrócona - cofnij obrót
+      card.style.transform = 'rotateY(0deg)';
+      // Usuń kartę z listy obróconych kart
+      flippedCards = flippedCards.filter(flippedCard => flippedCard !== card);
+    } else {
+      // Karta nie jest jeszcze obrócona
+      if (flippedCards.length < 1) {
+        // Obróć kartę tylko jeśli mamy mniej niż dwie obrócone karty
+        card.style.transform = 'rotateY(180deg)';
+        // Dodaj kartę do listy obróconych kart
+        flippedCards.push(card);
+      } else {
+        // Mamy już dwie obrócone karty - cofnij obrót wszystkich kart i zresetuj listę
+        cards.forEach(otherCard => {
+          otherCard.style.transform = 'rotateY(0deg)';
+        });
+        flippedCards = [];
+      }
+    }
+  });
+});
+  
+  
+  
+  
+  
